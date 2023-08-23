a01b6e339e6c966d4bd288a246bd8bca619d478c
Yochana Mythri Gangyada <gangyadayochanamythri@gmail.com>
2023-08-23 20:44:00 +0530

    comments added

--- first.cpp
+++ first.cpp
@@ -106,6 +106,6 @@ int main()
         cout << "the required logarithmic value is:" << (x - (x*x/2) + (x*x*x/3) - (x*x*x*x/4) + (x*x*x*x*x/5) - (x*x*x*x*x*x/6) + (x*x*x*x*x*x*x/7)) << endl;
         }
     } while (selection != 'q' && selection != 'Q');
-
+//comment add jmcbdd
     return 0;
 }
